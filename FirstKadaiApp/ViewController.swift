4a5930de6ce504bc636ce566a95f57950b5b8d99
yuumi.sadamatsu <yuumisadamatsu@gmail.com>
2023-11-28 05:43:55 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 貞松優海 on 2023/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
